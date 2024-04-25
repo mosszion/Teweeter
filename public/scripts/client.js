be9e738ea1b847e9cea55caaaa594d97af5bb084
Mussie <musdzion@gmail.com>
2024-04-25 06:43:11 -0400

    added renderTweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,17 +7,54 @@
 
 $(() => {
 
-  // Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
+//   // Test / driver code (temporary). Eventually will get this from the server.
+// const tweetData = {
+//   "user": {
+//     "name": "Newton",
+//     "avatars": "https://i.imgur.com/73hZDYK.png",
+//       "handle": "@SirIsaac"
+//     },
+//   "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//   "created_at": 1461116232227
+// }
+// Fake data taken from initial-tweets.json
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
       "handle": "@SirIsaac"
     },
-  "content": {
+    "content": {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
-  "created_at": 1461116232227
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+//create renderTweets function
+//loops through a list of tweets
+//calls createTweetElement for each tweet
+//takes return value and appends to the tweets container
+  const renderTweets = function(tweets) {
+    //looping through list of tweets
+    for(let tweet of tweets) {
+      return createTweetElement(tweet)
+    }
+
 }
 
 // const createTweetElement = function (obj) {
@@ -32,23 +69,23 @@ function createTweetElement(tweet) {
   const html = `
     <header>
        <div class="name-head">
-          <span ><img src="${tweetData.user.avatars}" class="avatar"></span>
-          <span class="username">${tweetData.user.name}</span>
+          <span ><img src="${tweet.user.avatars}" class="avatar"></span>
+          <span class="username">${tweet.user.name}</span>
        </div>
        <div>
-            <span class="handle">${tweetData.user.handle}</span>
+            <span class="handle">${tweet.user.handle}</span>
        </div>
     </header>
 
     <p>
-      ${tweetData.content.text}
+      ${tweet.content.text}
     </p>
     
     <footer >
             <div class="line"></div>
             <div class="day-icon">
               <div class="days">
-                <span>${tweetData.created_at}</span>
+                <span>${tweet.created_at}</span>
               </div>
               <div class="icons">
                
@@ -71,11 +108,12 @@ function createTweetElement(tweet) {
   return $tweet;
 }
 
-const $tweet = createTweetElement(tweetData);
+const $tweets = renderTweets(data);
+// const $tweet = createTweetElement(tweetData);
 
 // // Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('article').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+console.log($tweets); // to see what it looks like
+$('#tweets-container').append($tweets); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
 
 
 })
