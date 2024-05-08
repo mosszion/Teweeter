461ab6526eeb9c46d264bf69eba28b619aaaef3c
Mussie <musdzion@gmail.com>
2024-05-08 17:25:23 -0400

    add some corrections

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  $("#tweetText").on("keyup", function() {
+  $("#tweetText").on("keydown", function() {
   let noChar = $(this).val().length;
    let updatedNo = 140 - noChar;
    if (updatedNo < 0) {
@@ -9,9 +9,15 @@ $(document).ready(function() {
    } else {
 
      $(".counter").text(updatedNo).removeClass("belowZero") ;
-   }
+    } 
   
+   
 });
+$("#tButton").on("click", function() {
+let updatedNo = 140;
+ $(".counter").text(updatedNo);
+});
+  
 
 
 });
